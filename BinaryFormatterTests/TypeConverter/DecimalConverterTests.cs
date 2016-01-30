346d2ba4cdd0353a9ca069a48ef474ca1f585ce7
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-01-30 11:55:26 +0100

    Decimal formatter

---
+++ BinaryFormatterTests/TypeConverter/DecimalConverterTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using BinaryFormatter.TypeConverter;
+using Xunit;
+
+namespace BinaryFormatterTests.TypeConverter
+{
+    public class DecimalConverterTests
+    {
+        [Fact]
+        public void CanCorrectSerialize()
+        {
+            decimal value = decimal.MaxValue;
+            DecimalConverter converter = new DecimalConverter();
+            byte[] bytes = converter.Serialize(value);
+
+            int size = BitConverter.ToInt32(bytes, 0);
+            string valuestring = Encoding.UTF8.GetString(bytes, sizeof(int), bytes.Length - sizeof(int));
+            decimal valueFromBytes = decimal.Parse(valuestring);
+
+            Assert.Equal(size, valuestring.Length);
+            Assert.Equal(valueFromBytes, value);
+        }
+    }
+}
---
+++ src/BinaryFormatter/TypeConverter/DecimalConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace BinaryFormatter.TypeConverter
+{
+    internal class DecimalConverter : BaseTypeConverter<decimal>
+    {
+        private int Size { get; set; } = 0;
+
+        protected override byte[] ProcessSerialize(decimal obj)
+        {
+            string sdecimal = obj.ToString("F");
+            Size = sdecimal.Length;
+            return Encoding.UTF8.GetBytes(sdecimal);
+        }
+
+        protected override int GetTypeSize()
+        {
+            return Size;
+        }
+    }
+}
