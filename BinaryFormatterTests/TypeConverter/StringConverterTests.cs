7203eb960469b4d183583f239ef583c0616abc84
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-01-30 11:47:20 +0100

    String converter

---
+++ BinaryFormatterTests/TypeConverter/StringConverterTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using BinaryFormatter.TypeConverter;
+using Xunit;
+
+namespace BinaryFormatterTests.TypeConverter
+{
+    public class StringConverterTests
+    {
+        [Fact]
+        public void CanCorrectSerialize()
+        {
+            string value = "Lorem ipsum";
+            StringConverter converter = new StringConverter();
+            byte[] bytes = converter.Serialize(value);
+
+            int size = BitConverter.ToInt32(bytes, 0);
+            string valueFromBytes = Encoding.UTF8.GetString(bytes, sizeof(int), bytes.Length - sizeof(int));
+
+            Assert.Equal(size, value.Length);
+            Assert.Equal(valueFromBytes, value);
+        }
+    }
+}
---
+++ src/BinaryFormatter/TypeConverter/StringConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace BinaryFormatter.TypeConverter
+{
+    internal class StringConverter : BaseTypeConverter<string>
+    {
+        private int Size { get; set; } = 0;
+
+        protected override byte[] ProcessSerialize(string obj)
+        {
+            Size = obj.Length;
+            return Encoding.UTF8.GetBytes(obj);
+        }
+
+        protected override int GetTypeSize()
+        {
+            return Size;
+        }
+    }
+}
