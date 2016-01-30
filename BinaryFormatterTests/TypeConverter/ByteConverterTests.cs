39cbfc26383bfac8a50b16d4a2a95a2fc72e924d
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-01-30 11:00:24 +0100

    Byte converter

---
+++ BinaryFormatterTests/TypeConverter/ByteConverterTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BinaryFormatter.TypeConverter;
+using Xunit;
+
+namespace BinaryFormatterTests.TypeConverter
+{
+    public class ByteConverterTests
+    {
+        [Fact]
+        public void CanCorrectSerializeShort()
+        {
+            byte value = byte.MaxValue;
+            ByteConverter converter = new ByteConverter();
+            byte[] bytes = converter.Serialize(value);
+
+            int size = BitConverter.ToInt32(bytes, 0);
+            int valueFromBytes = BitConverter.ToInt16(bytes, sizeof (int));
+
+            Assert.Equal(size, sizeof(byte));
+            Assert.Equal(valueFromBytes, value);
+        }
+    }
+}
---
+++ src/BinaryFormatter/TypeConverter/ByteConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BinaryFormatter.TypeConverter
+{
+    internal class ByteConverter : BaseTypeConverter<byte>
+    {
+        protected override byte[] ProcessSerialize(byte obj)
+        {
+            return BitConverter.GetBytes(obj);
+        }
+
+        protected override int GetTypeSize()
+        {
+            return sizeof (byte);
+        }
+    }
+}
