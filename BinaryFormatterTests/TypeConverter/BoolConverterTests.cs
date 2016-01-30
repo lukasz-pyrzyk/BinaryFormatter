5177b5f81b032588461985479e215fb6bd385125
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-01-30 11:37:41 +0100

    Bool converter

---
+++ BinaryFormatterTests/TypeConverter/BoolConverterTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BinaryFormatter.TypeConverter;
+using Xunit;
+
+namespace BinaryFormatterTests.TypeConverter
+{
+    public class BoolConverterTests
+    {
+        [Fact]
+        public void CanCorrectSerialize()
+        {
+            bool value = false;
+            BoolConverter converter = new BoolConverter();
+            byte[] bytes = converter.Serialize(value);
+
+            int size = BitConverter.ToInt32(bytes, 0);
+            bool valueFromBytes = BitConverter.ToBoolean(bytes, sizeof (int));
+
+            Assert.Equal(size, sizeof(bool));
+            Assert.Equal(valueFromBytes, value);
+        }
+    }
+}
---
+++ src/BinaryFormatter/TypeConverter/BoolConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BinaryFormatter.TypeConverter
+{
+    internal class BoolConverter : BaseTypeConverter<bool>
+    {
+        protected override byte[] ProcessSerialize(bool obj)
+        {
+            return BitConverter.GetBytes(obj);
+        }
+
+        protected override int GetTypeSize()
+        {
+            return sizeof (bool);
+        }
+    }
+}
