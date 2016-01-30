250beee30b8eb580adb5b9502c087cf0d0c1fdf1
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-01-30 11:37:34 +0100

    Char converter

---
+++ BinaryFormatterTests/TypeConverter/CharConverterTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BinaryFormatter.TypeConverter;
+using Xunit;
+
+namespace BinaryFormatterTests.TypeConverter
+{
+    public class CharConverterTests
+    {
+        [Fact]
+        public void CanCorrectSerialize()
+        {
+            char value = char.MaxValue;
+            CharConverter converter = new CharConverter();
+            byte[] bytes = converter.Serialize(value);
+
+            int size = BitConverter.ToInt32(bytes, 0);
+            char valueFromBytes = BitConverter.ToChar(bytes, sizeof (int));
+
+            Assert.Equal(size, sizeof(char));
+            Assert.Equal(valueFromBytes, value);
+        }
+    }
+}
---
+++ src/BinaryFormatter/TypeConverter/CharConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BinaryFormatter.TypeConverter
+{
+    internal class CharConverter : BaseTypeConverter<char>
+    {
+        protected override byte[] ProcessSerialize(char obj)
+        {
+            return BitConverter.GetBytes(obj);
+        }
+
+        protected override int GetTypeSize()
+        {
+            return sizeof(char);
+        }
+    }
+}
