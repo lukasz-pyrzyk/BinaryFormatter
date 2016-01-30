7201db13d74d483d771c3fb7d736afc377770c8d
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-01-30 10:59:47 +0100

    SByte converter

---
+++ BinaryFormatterTests/TypeConverter/SByteConverterTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BinaryFormatter.TypeConverter;
+using Xunit;
+
+namespace BinaryFormatterTests.TypeConverter
+{
+    public class SByteConverterTests
+    {
+        [Fact]
+        public void CanCorrectSerializeShort()
+        {
+            sbyte value = sbyte.MaxValue;
+            SByteConverter converter = new SByteConverter();
+            byte[] bytes = converter.Serialize(value);
+
+            int size = BitConverter.ToInt32(bytes, 0);
+            int valueFromBytes = BitConverter.ToInt16(bytes, sizeof (int));
+
+            Assert.Equal(size, sizeof(sbyte));
+            Assert.Equal(valueFromBytes, value);
+        }
+    }
+}
