8e8353e78238e3b901b28b4c719d3756fb4d7bc9
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-07-06 22:21:51 +0200

    Added benchmark project

---
+++ src/Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Benchmark
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from benchmark!");
+        }
+    }
+}
