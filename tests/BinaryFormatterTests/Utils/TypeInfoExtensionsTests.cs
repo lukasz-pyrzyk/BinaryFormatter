0561a65c2f6b613205acd90e27167472f10ab30f
Yury Permitin <ypermitin@outlook.com>
2017-07-11 23:13:20 +0500

    #40 Fixed problem with virtual properties and properties inherited from classes. Added unit tests.
    - Fixed problem with virtual properties and static propetries.
    - Only writable properties can be serialized/deserialized.
    - Added extension for "TypeInfo" type
    - Added unit tests.

---
+++ tests/BinaryFormatterTests/Utils/TypeInfoExtensionsTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Reflection;
+using BinaryFormatter.Utils;
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BinaryFormatterTests.Utils
+{
+    public class TypeInfoExtensionsTests
+    {
+        class Master
+        {
+            public string Name { get; set; }
+        }
+        class Slave : Master
+        {
+            public int Priority { get; set; }
+        }
+
+        [Fact(Skip = "Work in progress")]
+        public void GetAllConstructors()
+        {
+
+        }
+
+        [Fact(Skip = "Work in progress")]
+        public void GetAllEvents()
+        {
+
+        }
+
+        [Fact(Skip = "Work in progress")]
+        public void GetAllFields()
+        {
+
+        }
+
+        [Fact(Skip = "Work in progress")]
+        public void GetAllMembers()
+        {
+
+        }
+
+        [Fact(Skip = "Work in progress")]
+        public void GetAllMethods()
+        {
+
+        }
+
+        [Fact(Skip = "Work in progress")]
+        public void GetAllNestedTypes()
+        {
+
+        }
+
+        [Fact]
+        public void GetAllProperties()
+        {
+            Slave TestObject = new Slave() { Name = "Test 1", Priority = 123 };
+            IEnumerable<PropertyInfo> allProperties = TestObject.GetType().GetTypeInfo().GetAllProperties();
+
+            Assert.Equal(allProperties.Count(), 2);
+        }
+    }
+}
--- tests/BinaryFormatterTests/WhenWorkingWith_Classes.cs
+++ tests/BinaryFormatterTests/WhenWorkingWith_Classes.cs
@@ -1,4 +1,7 @@
 ﻿using BinaryFormatter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace BinaryFormatterTests
@@ -24,6 +27,94 @@ namespace BinaryFormatterTests
             public int Int { get; set; }
             public double Double { get; set; }
             public string String { get; set; }
+        }
+
+        abstract class WithVirtualProperties
+        {
+            protected static List<WithVirtualProperties_Element> _elements = new List<WithVirtualProperties_Element>();
+            public static IReadOnlyCollection<WithVirtualProperties_Element> AllElements
+            {
+                get
+                {
+                    return _elements as IReadOnlyCollection<WithVirtualProperties_Element>;
+                }
+            }
+            public static IReadOnlyCollection<WithVirtualProperties_Group> AllGroups
+            {
+                get
+                {
+                    return _elements.Select(gr => gr.Group)
+                        .Distinct()
+                        .ToList() as IReadOnlyCollection<WithVirtualProperties_Group>;
+                }
+            }
+
+            public string Name { get; set; }
+
+            public WithVirtualProperties()
+            {
+            }
+            public WithVirtualProperties(string Name)
+            {
+                this.Name = Name;
+            }
+
+            public override string ToString()
+            {
+                return this.Name;
+            }
+        }
+
+        class WithVirtualProperties_Element : WithVirtualProperties, IDisposable
+        {
+            public WithVirtualProperties_Group Group { get; set; }
+
+            public WithVirtualProperties_Element()
+            {
+
+            }
+            public WithVirtualProperties_Element(string Name, WithVirtualProperties_Group Group):base(Name)
+            {
+                this.Group = Group;
+                _elements.Add(this);
+            }
+
+            public virtual IEnumerable<WithVirtualProperties_Group> Groups {
+                get
+                {
+                    return WithVirtualProperties.AllElements
+                        .Where(el => el == this)
+                        .Select(gr => gr.Group)
+                        .Distinct()
+                        .ToList() as IEnumerable<WithVirtualProperties_Group>;
+                }
+            }
+
+            public void Dispose()
+            {
+                _elements.Remove(this);
+            }
+        }
+
+        class WithVirtualProperties_Group : WithVirtualProperties
+        {
+            public WithVirtualProperties_Group()
+            {
+
+            }
+            public WithVirtualProperties_Group(string Name) : base(Name)
+            {
+            }
+
+            public virtual IEnumerable<WithVirtualProperties_Element> Elements {
+                get {
+
+                    return WithVirtualProperties.AllElements
+                        .Where(el => el.Group == this)
+                        .Select(el => el)
+                        .ToList() as IEnumerable<WithVirtualProperties_Element>;
+                }
+            }
         }
 
         [Fact]
@@ -54,6 +145,32 @@ namespace BinaryFormatterTests
             Assert.Equal(before.String, after.String);
             Assert.Equal(before.Int, after.Int);
             Assert.Equal(before.Double, after.Double);
+        }
+
+        [Fact]
+        public void CanWorkWith_ClassesWithoutCustomCtor_WithVirtualProperties_WithPublicSetter()
+        {
+            for (int iGroup = 1; iGroup <= 5; iGroup++)
+            {
+                string groupName = string.Format("Group {0}", iGroup);
+                WithVirtualProperties_Group currentGroup = new WithVirtualProperties_Group(groupName);
+
+                for (int iElement = 1; iElement <= 5; iElement++)
+                {
+                    string nameElement = string.Format("Element {0}-{1}", iGroup, iElement);
+                    WithVirtualProperties_Element currentElement = new WithVirtualProperties_Element(nameElement, currentGroup);
+                }
+            }
+
+            var formatter = new BinaryConverter();
+
+            foreach (WithVirtualProperties_Group group in WithVirtualProperties.AllGroups)
+            {
+                byte[] data = formatter.Serialize(group);
+                WithVirtualProperties_Group after = formatter.Deserialize<WithVirtualProperties_Group>(data);
+
+                Assert.Equal(group.Name, after.Name);
+            }
         }
     }
 }
