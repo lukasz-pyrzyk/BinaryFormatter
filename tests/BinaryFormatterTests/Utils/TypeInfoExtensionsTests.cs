322a11b247bceb9c417715492589aa0ebe8ef8a8
Yury Permitin <ypermitin@outlook.com>
2017-07-12 12:30:45 +0500

    Implemented tests for all extension methods

--- tests/BinaryFormatterTests/Utils/TypeInfoExtensionsTests.cs
+++ tests/BinaryFormatterTests/Utils/TypeInfoExtensionsTests.cs
@@ -3,6 +3,7 @@ using BinaryFormatter.Utils;
 using Xunit;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace BinaryFormatterTests.Utils
 {
@@ -11,46 +12,80 @@ namespace BinaryFormatterTests.Utils
         class Master
         {
             public string Name { get; set; }
+            public event EventHandler testEvent_1;
+
+            class Nested_1
+            {
+                Nested_1()
+                {
+                }
+            }
         }
         class Slave : Master
         {
             public int Priority { get; set; }
+            public event EventHandler testEvent_2;
+
+            class Nested_2
+            {
+                Nested_2()
+                {
+                }
+            }
         }
 
-        [Fact(Skip = "Work in progress")]
+        [Fact]
         public void GetAllConstructors()
         {
+            Slave TestObject = new Slave() { Name = "Test 1", Priority = 123 };
+            IEnumerable<ConstructorInfo> allConstructors = TestObject.GetType().GetTypeInfo().GetAllConstructors();
 
+            Assert.Equal(allConstructors.Count(), 3);
         }
 
-        [Fact(Skip = "Work in progress")]
+        [Fact]
         public void GetAllEvents()
         {
+            Slave TestObject = new Slave() { Name = "Test 1", Priority = 123 };
+            IEnumerable<EventInfo> allEvents = TestObject.GetType().GetTypeInfo().GetAllEvents();
 
+            Assert.Equal(allEvents.Count(), 2);
         }
 
-        [Fact(Skip = "Work in progress")]
+        [Fact]
         public void GetAllFields()
         {
+            Slave TestObject = new Slave() { Name = "Test 1", Priority = 123 };
+            IEnumerable<FieldInfo> allFields = TestObject.GetType().GetTypeInfo().GetAllFields();
 
+            Assert.Equal(allFields.Count(), 4);
         }
 
-        [Fact(Skip = "Work in progress")]
+        [Fact]
         public void GetAllMembers()
         {
+            Slave TestObject = new Slave() { Name = "Test 1", Priority = 123 };
+            IEnumerable<MemberInfo> allMembers = TestObject.GetType().GetTypeInfo().GetAllMembers();
 
+            Assert.Equal(allMembers.Count(), 32);
         }
 
-        [Fact(Skip = "Work in progress")]
+        [Fact]
         public void GetAllMethods()
         {
+            Slave TestObject = new Slave() { Name = "Test 1", Priority = 123 };
+            IEnumerable<MemberInfo> allMethods = TestObject.GetType().GetTypeInfo().GetAllMethods();
 
+            Assert.Equal(allMethods.Count(), 19);
         }
 
-        [Fact(Skip = "Work in progress")]
+        [Fact]
         public void GetAllNestedTypes()
         {
+            Slave TestObject = new Slave() { Name = "Test 1", Priority = 123 };
+            IEnumerable<MemberInfo> allNestedTypes = TestObject.GetType().GetTypeInfo().GetAllNestedTypes();
 
+            Assert.Equal(allNestedTypes.Count(), 2);
         }
 
         [Fact]
