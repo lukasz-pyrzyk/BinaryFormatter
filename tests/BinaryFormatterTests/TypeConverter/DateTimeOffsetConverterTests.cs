6c468868b363e2b2afdeab82c115b76871937031
Yury Permitin <ypermitin@outlook.com>
2017-09-04 23:55:14 +0500

    #30 Added unit test for DateTimeOffset type

---
+++ tests/BinaryFormatterTests/TypeConverter/DateTimeOffsetConverterTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BinaryFormatter;
+using Xunit;
+
+namespace BinaryFormatterTests.TypeConverter
+{
+    public class DateTimeOffsetConverterTests : ConverterTest<DateTimeOffset>
+    {
+        [Fact]
+        public void CanSerializeAndDeserialize()
+        {
+            RunTest();
+        }
+
+        public override DateTimeOffset Value => DateTimeOffset.MaxValue;
+    }
+}
