3dba0d60d05c6a390ee6e616ef649738811993a2
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-12-12 09:06:25 +0100

    Added fixture for enums

---
+++ tests/BinaryFormatterTests/WhenSerializingEnum.cs
@@ -0,0 +1,32 @@
+﻿using BinaryFormatter;
+using FluentAssertions;
+using Xunit;
+
+namespace BinaryFormatterTests
+{
+    public class WhenSerializingEnum
+    {
+        [Fact]
+        public void EnumCanBeSerializedAndDeserialized()
+        {
+            // Arrange
+            var gender = Gender.Male;
+
+            var converter = new BinaryConverter();
+            byte[] serialized = converter.Serialize(gender);
+
+            // act
+            var deserialized = converter.Deserialize<Gender>(serialized);
+
+            // assert
+            deserialized.Should().Be(gender);
+        }
+
+        private enum Gender
+        {
+            Unknown = 0,
+            Male = 1,
+            Female = 2
+        }
+    }
+}
