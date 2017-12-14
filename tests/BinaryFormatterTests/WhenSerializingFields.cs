76978fc43f56ee431f08d5bd45ca630aa33514ad
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-12-14 15:32:04 +0100

    Unit tests for serializing fields

---
+++ tests/BinaryFormatterTests/WhenSerializingFields.cs
@@ -0,0 +1,41 @@
+﻿using AutoFixture;
+using BinaryFormatter;
+using FluentAssertions;
+using Xunit;
+
+namespace BinaryFormatterTests
+{
+    public class WhenSerializingFields
+    {
+        private static readonly Fixture Fixture = new Fixture();
+
+        [Fact]
+        public void CanSerializeAndDeserialize()
+        {
+            var obj = Fixture.Create<StreamMessage>();
+
+            var converter = new BinaryConverter();
+
+            byte[] bytes = converter.Serialize(obj);
+            StreamMessage fromBytes = converter.Deserialize<StreamMessage>(bytes);
+
+            fromBytes.Should().NotBeNull();
+            fromBytes.StreamContent.Should().Be(obj.StreamContent);
+            fromBytes.StreamType.Should().Be(obj.StreamType);
+            fromBytes.StreamSize.Should().Be(obj.StreamSize);
+            fromBytes.StreamContent.Should().Be(obj.StreamContent);
+        }
+
+        public class StreamMessage
+        {
+            public string StreamName;
+            public int StreamType;
+            public float StreamSize;
+            public byte StreamContent;
+
+            public StreamMessage()
+            {
+            }
+        }
+    }
+}
