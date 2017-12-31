6dc3e1309961a94a3e90f8051e5cef9e14d9d5fc
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-12-31 14:12:36 +0100

    Cleanup of the test fixture

--- tests/BinaryFormatter.Tests/WhenWorkingWith_Classes.cs
+++ tests/BinaryFormatter.Tests/WhenWorkingWith_Classes.cs
@@ -1,37 +1,22 @@
 ﻿using System;
+using FluentAssertions;
 using Xunit;
+// ReSharper disable MemberCanBePrivate.Local
+// ReSharper disable UnusedAutoPropertyAccessor.Local
+// ReSharper disable UnusedMember.Local
 
 namespace BinaryFormatter.Tests
 {
     public class WhenWorkingWithClasses
     {
-        class WithoutCtor
-        {
-            public int Int { get; set; }
-            public double Double { get; set; }
-            public string String { get; set; }
-        }
-
-        class WithReadonlyProperties
-        {
-            public string Name { get; set; }
-            public DateTime BirthDay { get; set; }
-            public int Age => DateTime.Now.Year - BirthDay.Year;
-        }
-
         [Fact]
         public void CanWorkWith_ClassesWithoutCustomCtor_WithProperties_WithPublicSetter()
         {
             var before = new WithoutCtor { Int = 1, Double = 1, String = "lorem ipsum" };
 
-            var formatter = new BinaryConverter();
-            byte[] data = formatter.Serialize(before);
+            var after = TestHelper.SerializeAndDeserialize(before);
 
-            var after = formatter.Deserialize<WithoutCtor>(data);
-
-            Assert.Equal(before.String, after.String);
-            Assert.Equal(before.Int, after.Int);
-            Assert.Equal(before.Double, after.Double);
+            after.Should().BeEquivalentTo(before);
         }
 
         [Fact]
@@ -43,13 +28,23 @@ namespace BinaryFormatter.Tests
                 BirthDay = DateTime.Now.AddYears(-50)
             };
 
-            var formatter = new BinaryConverter();
-            byte[] data = formatter.Serialize(before);
-            var after = formatter.Deserialize<WithReadonlyProperties>(data);
+            var after = TestHelper.SerializeAndDeserialize(before);
 
-            Assert.Equal(before.Name, after.Name);
-            Assert.Equal(before.BirthDay, after.BirthDay);
-            Assert.Equal(before.Age, after.Age);
+            after.Should().BeEquivalentTo(before);
+        }
+
+        private class WithoutCtor
+        {
+            public int Int { get; set; }
+            public double Double { get; set; }
+            public string String { get; set; }
+        }
+
+        private class WithReadonlyProperties
+        {
+            public string Name { get; set; }
+            public DateTime BirthDay { get; set; }
+            public int Age => DateTime.Now.Year - BirthDay.Year;
         }
     }
 }
